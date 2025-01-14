94ec8bfd48a047bb45a090d479f006f797f42c6c
LathifNurHidayat <lathifnurhidayat09@gmail.com>
2025-01-14 07:27:42 +0700

    Project Setup

---
+++ Sistem-Manajemen-Booking-Bengkel/Helper/ConstStringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistem_Manajemen_Booking_Bengkel.Helper
+{
+    public class ConstStringHelper
+    {
+        public static string GetConn()
+        {
+            return "Server=(local); Database= ; TrustServerCertificate = true; Trusted_Connection = true;";
+        }
+    }
+}
---
+++ Sistem-Manajemen-Booking-Bengkel/View/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Sistem_Manajemen_Booking_Bengkel.View;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
